d44467c087899e9ab23945b95d1b7e01dd76d62a
art0575 <site@programmist.ru>
2021-06-03 00:54:24 +0300

    Задача решена

--- task_2.3/Main.java
+++ task_2.3/Main.java
@@ -15,7 +15,11 @@
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        for (int i = 1; i <= 10; i++) {
+            String num ="";
+            for (int j = 1; j <= i; j++) num += 8;
+            System.out.println(num);
+        }
 
     }
 }
