acd2989602a10410c2a4479ea0bfc1d8c665d615
art0575 <site@programmist.ru>
2021-06-02 04:00:24 +0300

    Задача решена

--- task_1.6/Main.java
+++ task_1.6/Main.java
@@ -1,9 +1,12 @@
 /*
 Пользователь вводит число, необходимо увеличить это число на 15% и вывести результат на экран
 */
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Ваш код
+        Scanner scan = new Scanner(System.in);
+        float count = scan.nextInt();
+        System.out.print(15.0*count/100+count);
+
     }
 }
