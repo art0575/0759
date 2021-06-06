cf851eb558c1e7cad1b824fc9d2e435960093c91
art0575 <site@programmist.ru>
2021-06-06 23:59:54 +0300

    Задача решена

--- task_2.9/Main.java
+++ task_2.9/Main.java
@@ -4,7 +4,11 @@
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        int[] nums = {3,6,8,5,4,2};
+        String answr  = "Нет";
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 5) answr  = "Да";
+        }
+        System.out.println(answr);
     }
 }
