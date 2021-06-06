0b061592dee36af46d49378bd65386db02010de7
art0575 <site@programmist.ru>
2021-06-06 21:29:55 +0300

    Задача решена

--- task_2.7/Main.java
+++ task_2.7/Main.java
@@ -1,10 +1,23 @@
 /*
 Задание: Дано число, например 31. Проверьте, что это число не делится ни на одно другое число кроме себя самого и единицы. То есть в нашем случае нужно проверить, что число 31 не делится на все числа от 2 до 30. Если число не делится - выведите 'false', а если делится - выведите 'true'.
 */
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int a = scanner.nextInt();
+        boolean b = false;
+
+        for (int i = 2; i < a; i++) {
+
+            if (a%i == 0){
+                b = true;
+                System.out.println("Деление без остатка: " + a + ":" + i);
+            }
+
+        }
+        System.out.println(b);
     }
 }
