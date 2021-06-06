e11033c68a02d0e5cca1c70f3de66dbfb780f5e2
art0575 <site@programmist.ru>
2021-06-06 07:05:57 +0300

    Задача решена

--- task_2.6/Main.java
+++ task_2.6/Main.java
@@ -2,9 +2,24 @@
 Задание: Даны переменные a и b. Проверьте, что a делится без остатка на b. Если это так - выведите 'Делится' и результат деления, иначе выведите 'Делится с остатком' и остаток от деления.
 */
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
 
+        int a, b;
+        Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите переменную a");
+            a = scanner.nextInt();
+            System.out.println("Введите переменную b");
+            b = scanner.nextInt();
+
+
+        if (a%b != 0) {
+            System.out.println("Делится с остатком: " + a%b);
+        } else {
+            System.out.println(" Делится: " + a/b);
+
+        }
     }
 }
