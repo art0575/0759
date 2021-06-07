8d361885b5d0787ae47e4bf2e4156a35372f37eb
art0575 <site@programmist.ru>
2021-06-07 03:34:47 +0300

    Задача решена

--- task_2.11/Main.java
+++ task_2.11/Main.java
@@ -4,7 +4,20 @@
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        int[] nums = {2, 3, 1, 4, 3, 6, 1};
+        int sum = 0;
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (sum < 11) {
+                result++;
+                sum = sum + nums[i];
+            }
+        }
+        if (sum > 10) {
+            System.out.println("Надо сложить "+result+" элементов с начала массива, чтобы сумма получилась больше 10-ти");
+        } else {
+            System.out.println("Сумма чисел массива меньше или равна 10");
+        }
 
     }
 }
