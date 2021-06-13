8dde340f24711031acab3538db4ddbd6a8e993b2
art0575 <site@programmist.ru>
2021-06-13 11:01:14 +0300

    Задача решена

--- task_4.2/Main.java
+++ task_4.2/Main.java
@@ -14,11 +14,42 @@
 
 public class Main {
     public static void main(String[] args) {
+        Dog dog1 = new Dog(); // наследует и имя и возраст
+        Dog dog2 = new Dog(); // не наследует
+        Dog dog3 = new Dog(); // наследует имя
+        Dog dog4 = new Dog(); // наследует возраст
+        dog2.setName("Мурзик");
+        dog2.setAge(3);
+        dog3.setAge(5);
+        dog4.setName("Алабай");
+        System.out.println(dog1.getName()+" "+dog1.getAge());
+        System.out.println(dog2.getName()+" "+dog2.getAge());
+        System.out.println(dog3.getName()+" "+dog3.getAge());
+        System.out.println(dog4.getName()+" "+dog4.getAge());
 
     }
 }
 
-public class Dog {
-    //напишите тут ваш код
+//public class Dog {
+class Dog {
+
+    private String name = "Тузик";
+    private int age = 2;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getAge() {
+        return age;
+
+    }
+
 }
 
