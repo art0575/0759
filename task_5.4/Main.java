88878a9daf0a598d5b297f370ea97b8b0b58ba80
art0575 <site@programmist.ru>
2021-06-17 18:38:15 +0300

    Задача решена

--- task_5.4/Main.java
+++ task_5.4/Main.java
@@ -1,5 +1,4 @@
-  
-/* 
+/*
 1. Ввести путь к файлу с консоли.
 2. Прочитать из него набор чисел.
 3. Вывести на консоль только четные, отсортированные по возрастанию.
@@ -19,8 +18,45 @@
 10
 */
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        // напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите путь к файлу: ");
+        String filePath = scanner.nextLine();
+        ArrayList<Integer> file_nums = new ArrayList<>();
+
+        try {
+            FileInputStream fis = new FileInputStream(filePath);
+            int i;
+            String num = "";
+            try {
+                while (true) {
+                    i = fis.read();
+                    if (i == 13 || i == 10 || i == -1) {
+                        if (num != "") {
+                            file_nums.add(Integer.parseInt(num));
+                            num = "";
+                        }
+                        if (i== -1) break;
+                    } else num += (char)i;
+                }
+                System.out.println("Набор всех чисел: \n"+file_nums);
+                Collections.sort(file_nums);
+                int index = 0;
+                while (file_nums.size() > index){
+                    if (file_nums.get(index)%2 !=0) file_nums.remove(index);
+                    else index++;
+                }
+                System.out.println("Набор четных чисел, сортрованные по возрастанию: \n"+file_nums);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
     }
 }
