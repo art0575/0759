b1ce40dc22f027330a5b2b4d38782889a64d75ec
art0575 <site@programmist.ru>
2021-06-17 03:14:21 +0300

    Задача решена

--- task_4.7/Main.java
+++ task_4.7/Main.java
@@ -1,24 +1,33 @@
 /*
 Реализуй в классе Fox интерфейс Animal.
 Учти, что создавать дополнительные классы и удалять методы нельзя!
-
 Требования:
 •	Интерфейс Animal должен быть реализован в классе Fox.
 •	В интерфейсе Animal нужно объявить метод getColor.
 •	Дополнительные классы или интерфейсы создавать нельзя.
 */
 
+import java.awt.Color;
 public class Main {
     public static void main(String[] args){
+        Fox fox = new Fox();
+        System.out.println (fox.getName());
+        System.out.println (fox.getColor());
+
+
     }
 }
 
 interface Animal {
     Color getColor();
 }
-    
-class Fox {
+
+class Fox implements Animal {
     public String getName() {
         return "Fox";
+    }
+
+    public Color getColor() {
+        return Color.red;
     }
 }
