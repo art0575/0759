07e9e66d01c2ee1fef7184b1497208674423c6ea
art0575 <site@programmist.ru>
2021-06-17 14:28:00 +0300

    Задача решена

--- task_5.3/Main.java
+++ task_5.3/Main.java
@@ -1,11 +1,32 @@
 /*
-* Задание: Дана коллекция с числами. Запишите в новую коллекцию только те числа, которые больше нуля и меньше 10-ти.
-* Коллекции вы создаёте сами
-*/
+ * Задание: Дана коллекция с числами. Запишите в новую коллекцию только те числа, которые больше нуля и меньше 10-ти.
+ * Коллекции вы создаёте сами
+ */
 
-import java.util.ArrayList;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-    
+
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        ArrayList<Integer> newCollection = new ArrayList<Integer>();
+
+        numbers.add(-2);
+        numbers.add(11);
+        numbers.add(5);
+        numbers.add(-18);
+        numbers.add(4);
+        numbers.add(7);
+        numbers.add(13);
+        numbers.add(3);
+
+        for (Integer i : numbers) {
+            if (i > 0 && i < 10)  newCollection.add (i);
+        };
+
+
+        System.out.println("Исходная коллекция с числами:\n" + numbers+"\n");
+        System.out.println("Коллекция чисел больше нуля и меньше 10-ти:\n" + newCollection);
+
     }
 }
